2dc8be8b0b5c27a7b78f0d94c80dab9552f02581
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2022-02-23 21:34:54 +0000

    added the sudo permissions

--- deploy/templates/user-data.sh
+++ deploy/templates/user-data.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y curl zip
 sudo apt install -y gettext-base moreutils
 sudo apt-get update
 sudo apt-get install cron
-chmod +x /home/ubuntu/ec2-caller.sh
+sudo chmod +x /home/ubuntu/ec2-caller.sh
 
 crontab<<EOF
 10,20,30,40,50,00 * * * * /home/ubuntu/ec2-caller.sh
