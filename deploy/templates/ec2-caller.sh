b26d63c72d014df85824687e1aaa83383fcc52a1
Balamurugan.Balasubramanian <kbcbala@gmail.com>
2022-02-25 00:32:30 +0000

    fixed the key issue

--- deploy/templates/ec2-caller.sh
+++ deploy/templates/ec2-caller.sh
@@ -2,8 +2,6 @@
 
 
 export CIRCLECI_TOKEN=$(echo ${CIRCLECI_TOKEN})
-export CIRCLECI_BR=$(echo ${GIT_BRANCH})
-
 
 sudo curl -u ${CIRCLECI_TOKEN}: -X POST --header "Content-Type: application/json" -d '{
    "branch": "${CIRCLE_BRANCH}",
